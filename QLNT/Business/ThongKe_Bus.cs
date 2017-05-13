21ee8fa06311f91c4cbd8285a8e9aa28bb72f98a
ANONYMOUS\binhboong1001 <vuontoidinhcao6789@gmail.com>
2017-05-13 10:08:05 +0700

    Test

---
+++ QLNT/Business/ThongKe_Bus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNT.LinQToSQL;
+namespace QLNT.Business
+{
+  public  class ThongKe_Bus
+  {
+      QLNTDataContext dtcontent = new QLNTDataContext();
+      public class ThongKeKhu
+      {
+           public String TenKhu { get; set; }
+           public int SoNguoi { get; set; }
+      }
+
+      public class ThongKeNamNu_
+      {
+         public String GioiTinh { get; set; }
+         public int TongSo { get; set; }
+      }
+
+      /* -------------------------------------------------------------------------------------------*/
+        public  List<ThongKeKhu> SiSoTheoKhu()
+        {
+            var ob = dtcontent.ExecuteQuery<ThongKeKhu>("exec [dbo].[NT_SP_ThongKeSoNguoiTheoKhu]");
+               return ob.ToList<ThongKeKhu>();
+        }
+        public List<ThongKeNamNu_> ThongKeNamNu()
+        {
+            var ob = dtcontent.ExecuteQuery<ThongKeNamNu_>("exec [dbo].[NT_SP_ThongKeTheoGioiTinh]");
+            return ob.ToList<ThongKeNamNu_>();
+        }
+       
+    }
+}
---
+++ QLNT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNT
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);          
+            Form.Systems.RunApplication run = new Form.Systems.RunApplication();
+
+            // chay cac form test o day
+            // Application.Run(new Form.Systems.RunApplication());
+            
+        }
+    }
+}
